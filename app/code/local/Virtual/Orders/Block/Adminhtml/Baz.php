6c0366faa2541ce4dcd3e894383126b5c60a1bcb
demoffon <dmitroch7@gmail.com>
2016-05-09 06:22:04 +0300

    1

---
+++ app/code/local/Virtual/Orders/Block/Adminhtml/Baz.php
@@ -0,0 +1,14 @@
+<?php
+class Virtual_Orders_Block_Adminhtml_Baz extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+    public function __construct()
+    {
+        // The blockGroup must match the first half of how we call the block, and controller matches the second half
+        // ie. foo_bar/adminhtml_baz
+        $this->_blockGroup = 'virtual_orders';
+        $this->_controller = 'adminhtml_baz';
+        $this->_headerText = $this->__('Baz');
+
+        parent::__construct();
+    }
+}
